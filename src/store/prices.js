3a5320f38065862eea79fd25ca841f957f80573e
Chiaki-xps <1490295446@qq.com>
2022-04-18 17:38:59 +0800

    价格文件

---
+++ src/store/prices.js
@@ -0,0 +1,35 @@
+// 视频包装
+export const videoPrice = 42000
+
+// VR无缝穿梭式区位
+export const vrPrice = 48000
+
+// 是日夜景切换
+export const xuShiPrice = 20000
+
+// 不日夜切换
+export const noXuShiPrice = 12000
+
+// 虚实结合全景鸟瞰
+export const niaoKanPrice1 = 12000
+export const niaoKanPrice2 = 20000
+export const niaoKanPrice3 = 26000
+
+// 360沙盘
+export const shaPanPrice = 8000
+// VR沙盘
+export const vrShaPanPrice = 16000
+
+// 720三维全景户型漫游
+export const quanJingHousePrice = 4000
+// 720全景实拍户型漫游
+export const shiPaiHousePrice = 1500
+// VR户型套装
+export const vrHpousePrice = 300
+
+// 三维渲染景观
+export const sanWeiPrice = 6000
+
+// 虚实结合景观
+export const xuShiJingPrice = 8000
+
